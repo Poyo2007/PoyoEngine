3fad59de8dd10d14c606a1d96bb58161d8682af7
Danintooshit <putakaputathatsmyberio@gmail.com>
2022-02-09 21:25:24 +0700

    well fuck you im about to sleep can you go any faster

--- source/Paths.hx
+++ source/Paths.hx
@@ -59,14 +59,14 @@ class Paths
 			}
 		}
 
-		// clear all sounds that are cached
-		for (key in currentTrackedSounds.keys()) {
-			if (!localTrackedAssets.contains(key) 
-			&& !dumpExclusions.contains(key) && key != null) {
-				Assets.cache.clear(key);
-				currentTrackedSounds.remove(key);
-			}
-		}	
+		// // clear all sounds that are cached
+		// for (key in currentTrackedSounds.keys()) {
+		// 	if (!localTrackedAssets.contains(key) 
+		// 	&& !dumpExclusions.contains(key) && key != null) {
+		// 		Assets.cache.clear(key);
+		// 		currentTrackedSounds.remove(key);
+		// 	}
+		// }	
 		// flags everything to be cleared out next unused memory clear
 		localTrackedAssets = [];
 		openfl.Assets.cache.clear("songs");
